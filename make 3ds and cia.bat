0e58363883e12f17277ff9a7a1533aba900e4531
Komas19-new <benoitbourcier@outlook.com>
2022-11-15 17:53:08 +0100

    Compiled folder

--- make 3ds and cia.bat
+++ make 3ds and cia.bat
@@ -1,4 +1,7 @@
 3dstool -cvtf romfs romfs.bin --romfs-dir romfs
 makerom -f cci -o flappy.3ds -rsf gw_workaround.rsf -target d -exefslogo -elf flappy.elf -icon icon.bin -romfs romfs.bin
 makerom -f cia -o flappy.cia -elf flappy.elf -rsf cia_workaround.rsf -icon icon.bin -exefslogo -target t -romfs romfs.bin
+move flappy.cia Compiled/
+move flappy.elf Compiled/
+move flappy.3dsx Compiled/
 pause
